155aaf246764138eac3f9d8d5acb2776ef273792
Jesse Claven <jesse.claven@me.com>
2026-03-30 16:07:08 +0100

    feat(site): Add page titles

--- site/lib/site_web/controllers/page_controller.ex
+++ site/lib/site_web/controllers/page_controller.ex
@@ -2,14 +2,14 @@ defmodule SiteWeb.PageController do
   use SiteWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, page_title: "Home")
   end
 
   def features(conn, _params) do
-    render(conn, :features)
+    render(conn, :features, page_title: "Features")
   end
 
   def readers(conn, _params) do
-    render(conn, :readers)
+    render(conn, :readers, page_title: "Readers")
   end
 end
